8eb72eb88386d5fec7de938ae81baa123c7bd0dd
Radomir Wojtera <rwojtera@egnyte.com>
2018-03-20 16:18:54 +0100

    WIP Interfaces

--- example_04/example.ts
+++ example_04/example.ts
@@ -1,3 +1,19 @@
 interface IPerson {
     name: string;
+    age?: number;
+    foo?: (number: number) => string;
+    arr?: Array<string>
 }
+
+class Person implements IPerson {
+    [x: number]: String;
+}
+
+// class Person implements IPerson {
+//     public readonly name = "";
+//     arr = ["asd"];
+//
+//     foo(number: number): string {
+//         return "";
+//     }
+// }
--- readme.md
+++ readme.md
@@ -5,5 +5,7 @@ Plan:
 * class
     * show getters (avaiable only for es5+)
 * interface
+    * implements
+    * extends !!! :-)
 * module patterns
 * importing external libs (d.ts files)
